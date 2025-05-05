61a67d875d592b1a2a983ad066c4652c282583c2
georgeacquahjunior <165186128+georgeacquahjunior@users.noreply.github.com>
2025-05-05 21:33:11 +0000

    The user wanted 12 rather than 10

--- #1 Multiplication Generator/Multiplication_Generator.py
+++ #1 Multiplication Generator/Multiplication_Generator.py
@@ -12,5 +12,5 @@ Stretch Goal: Allow user to set custom range.
 input_by_user = (int(input('Enter a number to display the Multiplication table from (1 to 10) ')))
 
 # Display its multiplication table (1 to 10)
-for number in range(1, 11, 1):
+for number in range(1, 13, 1):
     print(f'{input_by_user} * {number} = {input_by_user * number}')
